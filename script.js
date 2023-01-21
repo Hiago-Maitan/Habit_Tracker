996104cfb918c7286e64b4ccbb8bd3e52d1a7759
Maitann <hiagomaitan@gmail.com>
2023-01-21 00:57:16 -0300

    fourth live class
    add function() add and save(),
    learned about localStorage

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const form = document.querySelector('form')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-06', '01-07', '01-08', '01-09', '01-10', '01-11'],
-    water: ['01-08', '01-07'],
-    food: ['01-08', '01-07'],
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists) {
+        alert("[ERRO] O dia adicionado já existe")
+        return
+    }
+
+    alert("Dia adicionado com sucesso !!")
+    nlwSetup.addDay(today)
 }
 
+function save() {
+    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
 
